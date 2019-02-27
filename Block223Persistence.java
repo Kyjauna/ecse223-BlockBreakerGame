fb97ce67dba8eaa99eab4b9cd08939aa64607552
sharonkat <40246059+sharonkat@users.noreply.github.com>
2019-02-27 13:49:37 -0500

    Update Block223Persistence.java

--- Block223Persistence.java
+++ Block223Persistence.java
@@ -1,5 +1,10 @@
 package ca.mcgill.ecse223.block.persistence;
 
 public class Block223Persistence {
-
+  
+  public static void save(Block223 block223) {
+    PersistenceObjectSTream.serialize(block223); 
+  }
+  
+  
 }
