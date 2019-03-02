15f2914e4230c79f3c12145bce3a1f0ad2611346
sharonkat <sharon.kattar@mail.mcgill.ca>
2019-03-02 11:54:40 -0500

    Changed name of one page to GameLevelPage, CreateGamePage action listeners done except for save button, and work in progress YouAreAnAdminPage

--- src/ca/mcgill/ecse223/block/view/CreateGamePage.java
+++ src/ca/mcgill/ecse223/block/view/CreateGamePage.java
@@ -62,58 +62,13 @@ public class CreateGamePage {
 		frame = new JFrame();
 		frame.getContentPane().setBackground(new Color(0, 0, 51));
 		
-		JButton btnNewButton = new JButton("NUMBER OF LEVELS");
-		btnNewButton.setFont(new Font("Monospaced", Font.PLAIN, 11));
-		btnNewButton.setBackground(new Color(230, 230, 250));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JButton btnHeightPlayArea = new JButton("#BLOCKS per level");
-		btnHeightPlayArea.setFont(new Font("Monospaced", Font.PLAIN, 11));
-		btnHeightPlayArea.setBackground(new Color(230, 230, 250));
-		btnHeightPlayArea.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JButton btnMinXBall = new JButton("MIN X BALL SPEED");
-		btnMinXBall.setFont(new Font("Monospaced", Font.PLAIN, 11));
-		btnMinXBall.setBackground(new Color(230, 230, 250));
-		btnMinXBall.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JButton btnMinYBall = new JButton("MIN Y BALL SPEED");
-		btnMinYBall.setFont(new Font("Monospaced", Font.PLAIN, 11));
-		btnMinYBall.setBackground(new Color(230, 230, 250));
-		
-		JButton btnSpeedIncreaseFactor = new JButton("SPEED INCREASE FACTOR");
-		btnSpeedIncreaseFactor.setBackground(new Color(230, 230, 250));
-		btnSpeedIncreaseFactor.setFont(new Font("Monospaced", Font.PLAIN, 9));
-		
-		JButton btnMinPaddleLength = new JButton("MIN PADDLE LENGTH");
-		btnMinPaddleLength.setBackground(new Color(230, 230, 250));
-		btnMinPaddleLength.setFont(new Font("Monospaced", Font.PLAIN, 10));
-		btnMinPaddleLength.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JButton btnMaxPaddleLength = new JButton("MAX PADDLE LENGTH");
-		btnMaxPaddleLength.setBackground(new Color(230, 230, 250));
-		btnMaxPaddleLength.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnMaxPaddleLength.setFont(new Font("Monospaced", Font.PLAIN, 10));
+		/* Build Button */
 		
 		JButton btnBuild = new JButton("BUILD");
 		btnBuild.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				// this action should take you to the GamePage
+				// These lines assign the values of the game specifications.
 				
 				String stringNumberOfLevels = numberOfLevelsTxt.getText();
 				int numberOfLevels = Integer.parseInt(stringNumberOfLevels);
@@ -141,6 +96,9 @@ public class CreateGamePage {
 				} catch (InvalidInputException e1) {
 					e1.printStackTrace();
 				}
+				 
+				GameLevelPage gamePage = new GameLevelPage(); 	// Launches a new page (of type GameLevel)
+				gamePage.frame.setVisible(true);				// this is the frame from GameLevelPage, so their classes need to be public
 				
 			}
 		});
@@ -182,79 +140,151 @@ public class CreateGamePage {
 		maxPaddleLengthTxt.setForeground(new Color(169, 169, 169));
 		maxPaddleLengthTxt.setColumns(10);
 		
-		JLabel lblCreateNewGame = new JLabel("CREATE NEW GAME");
+		JLabel lblCreateNewGame = new JLabel("UPDATE GAME");
 		lblCreateNewGame.setFont(new Font("Monospaced", Font.BOLD, 25));
-		lblCreateNewGame.setForeground(new Color(64, 224, 208));
+		lblCreateNewGame.setForeground(new Color(153, 255, 255));
 		lblCreateNewGame.setBackground(new Color(70, 130, 180));
+		
+		JLabel lblNumberOfLevels = new JLabel("Number of Levels");
+		lblNumberOfLevels.setFont(new Font("Monospaced", Font.BOLD, 13));
+		lblNumberOfLevels.setForeground(new Color(204, 255, 255));
+		
+		JLabel lblblocksPerLevle = new JLabel("#BLOCKS per level");
+		lblblocksPerLevle.setFont(new Font("Monospaced", Font.BOLD, 13));
+		lblblocksPerLevle.setForeground(new Color(204, 255, 255));
+		
+		JLabel lblMinXBall = new JLabel("MIN X BALL SPEED");
+		lblMinXBall.setForeground(new Color(204, 255, 255));
+		lblMinXBall.setFont(new Font("Monospaced", Font.BOLD, 13));
+		
+		JLabel lblMinYBall = new JLabel("MIN Y BALL SPEED");
+		lblMinYBall.setForeground(new Color(204, 255, 255));
+		lblMinYBall.setFont(new Font("Monospaced", Font.BOLD, 13));
+		
+		JLabel lblSpeedIncreaseFactor = new JLabel("SPEED INCREASE FACTOR");
+		lblSpeedIncreaseFactor.setForeground(new Color(204, 255, 255));
+		lblSpeedIncreaseFactor.setFont(new Font("Monospaced", Font.BOLD, 11));
+		
+		JLabel lblMinPaddleLength = new JLabel("MIN PADDLE LENGTH");
+		lblMinPaddleLength.setForeground(new Color(204, 255, 255));
+		lblMinPaddleLength.setFont(new Font("Monospaced", Font.BOLD, 13));
+		
+		JLabel lblMaxPaddleLength = new JLabel("MAX PADDLE LENGTH");
+		lblMaxPaddleLength.setForeground(new Color(204, 255, 255));
+		lblMaxPaddleLength.setFont(new Font("Monospaced", Font.BOLD, 13));
+		
+		
+		/* Save button */
+		
+		JButton btnSave = new JButton("SAVE");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				// Save the game settings
+				
+				String stringNumberOfLevels = numberOfLevelsTxt.getText();
+				int numberOfLevels = Integer.parseInt(stringNumberOfLevels);
+				
+				String stringBlocksPerLevel = blocksPerLevelTxt.getText();
+				int blocksPerLevel = Integer.parseInt(stringBlocksPerLevel);
+								
+				String stringXBallSpeed = xBallSpeedTxt.getText();
+				int xBallSpeed = Integer.parseInt(stringXBallSpeed);
+				
+				String stringYBallSpeed = yBallSpeedTxt.getText();
+				int yBallSpeed = Integer.parseInt(stringYBallSpeed);
+				
+				String stringSpeedIncreaseFactor = speedIncreaseFactorTxt.getText();
+				double speedIncreaseFactor = Double.parseDouble(stringSpeedIncreaseFactor);
+				
+				String stringMinPaddleLength = minPaddleLengthTxt.getText();
+				int minPaddleLength = Integer.parseInt(stringMinPaddleLength);
+				
+				String stringMaxPaddleLength = maxPaddleLengthTxt.getText();
+				int maxPaddleLength = Integer.parseInt(stringMaxPaddleLength);
+				
+			}
+		});
+		
+		btnSave.setFont(new Font("Monospaced", Font.BOLD, 15));
+		btnSave.setBackground(new Color(135, 206, 235));
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
+			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addContainerGap(261, Short.MAX_VALUE)
-					.addComponent(lblCreateNewGame, GroupLayout.PREFERRED_SIZE, 231, GroupLayout.PREFERRED_SIZE)
-					.addGap(234))
+					.addComponent(lblCreateNewGame, GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)
+					.addGap(286))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(109)
+					.addGap(115)
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addComponent(btnMaxPaddleLength, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
-						.addComponent(btnMinPaddleLength, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
-						.addComponent(btnSpeedIncreaseFactor, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
-						.addComponent(btnMinYBall, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
-						.addComponent(btnMinXBall, GroupLayout.DEFAULT_SIZE, 151, Short.MAX_VALUE)
-						.addGroup(Alignment.LEADING, groupLayout.createParallelGroup(Alignment.LEADING, false)
-							.addComponent(btnHeightPlayArea, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-							.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-					.addGap(18)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addComponent(blocksPerLevelTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
-						.addComponent(numberOfLevelsTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
-						.addComponent(xBallSpeedTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
-						.addComponent(yBallSpeedTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
-						.addComponent(speedIncreaseFactorTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
-						.addComponent(minPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
-						.addComponent(maxPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE))
+						.addComponent(btnBuild)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+										.addComponent(lblNumberOfLevels)
+										.addComponent(lblblocksPerLevle))
+									.addGap(27))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+										.addComponent(lblMinXBall, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblMinYBall, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblMaxPaddleLength, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblMinPaddleLength, GroupLayout.PREFERRED_SIZE, 136, GroupLayout.PREFERRED_SIZE)
+										.addComponent(btnSave, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 77, GroupLayout.PREFERRED_SIZE))
+									.addGap(18))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblSpeedIncreaseFactor, GroupLayout.PREFERRED_SIZE, 153, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED)))
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(blocksPerLevelTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
+								.addComponent(numberOfLevelsTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
+								.addComponent(xBallSpeedTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
+								.addComponent(yBallSpeedTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
+								.addComponent(speedIncreaseFactorTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
+								.addComponent(minPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
+								.addComponent(maxPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE))))
 					.addGap(181))
-				.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
-					.addGap(324)
-					.addComponent(btnBuild)
-					.addContainerGap(325, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(20)
 					.addComponent(lblCreateNewGame, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE)
-					.addGap(33)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+					.addGap(38)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(numberOfLevelsTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addComponent(lblNumberOfLevels))
 					.addGap(18)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnHeightPlayArea)
-						.addComponent(blocksPerLevelTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
+						.addComponent(blocksPerLevelTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblblocksPerLevle))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 						.addComponent(xBallSpeedTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnMinXBall))
-					.addGap(12)
+						.addComponent(lblMinXBall, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+					.addGap(14)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnMinYBall)
-						.addComponent(yBallSpeedTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
+						.addComponent(yBallSpeedTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblMinYBall, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnSpeedIncreaseFactor, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-						.addComponent(speedIncreaseFactorTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
+						.addComponent(speedIncreaseFactorTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblSpeedIncreaseFactor, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnMinPaddleLength)
-						.addComponent(minPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
+						.addComponent(minPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblMinPaddleLength, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnMaxPaddleLength, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
-						.addComponent(maxPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-					.addGap(52)
-					.addComponent(btnBuild)
-					.addContainerGap(63, Short.MAX_VALUE))
+						.addComponent(maxPaddleLengthTxt, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblMaxPaddleLength, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+					.addGap(55)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnBuild)
+						.addComponent(btnSave, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(64, Short.MAX_VALUE))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 	}
