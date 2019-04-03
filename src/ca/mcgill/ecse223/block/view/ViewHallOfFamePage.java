a7d3845a27c822b7c8eb984b822acac3c64e0cd2
Kyjauna Marshall <Kyjauna@Kyjaunas-MacBook-Air.local>
2019-04-03 11:15:45 -0400

    new hall of fame design

--- src/ca/mcgill/ecse223/block/view/ViewHallOfFamePage.java
+++ src/ca/mcgill/ecse223/block/view/ViewHallOfFamePage.java
@@ -104,48 +104,6 @@ public class ViewHallOfFamePage {
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(new Color(255, 255, 0));
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(new Color(0, 204, 255));
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(51, 255, 0));
-		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBackground(new Color(102, 0, 153));
-		
-		JPanel panel_5 = new JPanel();
-		panel_5.setBackground(Color.MAGENTA);
-		
-		JPanel panel_6 = new JPanel();
-		panel_6.setBackground(Color.YELLOW);
-		
-		JPanel panel_7 = new JPanel();
-		panel_7.setBackground(new Color(0, 204, 255));
-		
-		JPanel panel_9 = new JPanel();
-		panel_9.setBackground(Color.YELLOW);
-		
-		JPanel panel_10 = new JPanel();
-		panel_10.setBackground(new Color(0, 204, 255));
-		
-		JPanel panel_11 = new JPanel();
-		panel_11.setBackground(new Color(51, 255, 0));
-		
-		JPanel panel_12 = new JPanel();
-		panel_12.setBackground(new Color(102, 0, 153));
-		
-		JPanel panel_13 = new JPanel();
-		panel_13.setBackground(Color.MAGENTA);
-		
-		JPanel panel_8 = new JPanel();
-		panel_8.setBackground(Color.YELLOW);
-		
-		JPanel panel_14 = new JPanel();
-		panel_14.setBackground(new Color(0, 204, 255));
-		
-		JPanel panel_15 = new JPanel();
-		panel_15.setBackground(new Color(51, 255, 0));
-		
 		JDesktopPane layeredPane_1 = new JDesktopPane();
 		
 		JLayeredPane layeredPane = new JLayeredPane();
@@ -178,19 +136,64 @@ public class ViewHallOfFamePage {
 		
 		JLayeredPane layeredPane_2 = new JLayeredPane();
 		
-		JPanel panel_16 = new JPanel();
-		panel_16.setBackground(new Color(102, 0, 153));
-		
-		JPanel panel_17 = new JPanel();
-		panel_17.setBackground(new Color(51, 255, 0));
-		
 		JLabel lblHallOfFame = new JLabel("HALL OF FAME");
-		lblHallOfFame.setForeground(new Color(224, 255, 255));
+		lblHallOfFame.setForeground(Color.CYAN);
 		lblHallOfFame.setFont(projectfont32);
 		
 		JLabel lblTopTen = new JLabel("TOP TEN");
-		lblTopTen.setForeground(new Color(224, 255, 255));
+		lblTopTen.setForeground(Color.MAGENTA);
 		lblTopTen.setFont(projectfont22);
+		
+		JPanel panel_18 = new JPanel();
+		panel_18.setBackground(Color.CYAN);
+		
+		JPanel panel_19 = new JPanel();
+		panel_19.setBackground(Color.GREEN);
+		
+		JPanel panel_20 = new JPanel();
+		panel_20.setBackground(new Color(153, 50, 204));
+		
+		JPanel panel_21 = new JPanel();
+		panel_21.setBackground(Color.MAGENTA);
+		
+		JPanel panel_22 = new JPanel();
+		panel_22.setBackground(new Color(255, 255, 0));
+		
+		JPanel panel_23 = new JPanel();
+		panel_23.setBackground(Color.CYAN);
+		
+		JPanel panel_24 = new JPanel();
+		panel_24.setBackground(Color.GREEN);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(Color.CYAN);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(Color.GREEN);
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBackground(new Color(153, 50, 204));
+		
+		JPanel panel_10 = new JPanel();
+		panel_10.setBackground(Color.YELLOW);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(Color.MAGENTA);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setBackground(Color.CYAN);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setBackground(Color.GREEN);
+		
+		JPanel panel_8 = new JPanel();
+		panel_8.setBackground(new Color(153, 50, 204));
+		
+		JPanel panel_9 = new JPanel();
+		panel_9.setBackground(Color.MAGENTA);
+		
+		JPanel panel_11 = new JPanel();
+		panel_11.setBackground(new Color(224, 255, 255));
 		
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 		groupLayout.setHorizontalGroup(
@@ -199,85 +202,140 @@ public class ViewHallOfFamePage {
 					.addContainerGap()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(panel_9, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_10, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_11, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_12, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_13, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_8, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_14, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_15, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-							.addComponent(panel_16, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-							.addGroup(groupLayout.createSequentialGroup()
-								.addGap(259)
-								.addComponent(lblHallOfFame)
-								.addGap(96)
-								.addComponent(layeredPane_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-								.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-									.addComponent(lblErrorMessage, GroupLayout.PREFERRED_SIZE, 524, GroupLayout.PREFERRED_SIZE)
-									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
-										.addGap(69))))
-							.addGroup(groupLayout.createSequentialGroup()
-								.addComponent(panel, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(panel_18, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_20, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_22, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_24, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(panel_19, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+										.addComponent(panel_21, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+										.addComponent(panel_23, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE))
+									.addGap(107)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+										.addComponent(panel_11, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(lblHallOfFame, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+									.addGap(96)
+									.addComponent(layeredPane_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_8, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(panel_10, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+									.addContainerGap())
 								.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-									.addComponent(lblGame)
 									.addGroup(groupLayout.createSequentialGroup()
-										.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-										.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-										.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+										.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
+											.addGroup(groupLayout.createSequentialGroup()
+												.addComponent(panel_9, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+												.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+												.addComponent(btnLogout, GroupLayout.PREFERRED_SIZE, 87, GroupLayout.PREFERRED_SIZE)
+												.addGap(69))
+											.addGroup(groupLayout.createSequentialGroup()
+												.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+													.addComponent(panel_7, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+													.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE))
+												.addGap(368)))
+										.addGap(138))
+									.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
 										.addComponent(panel_6, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-										.addComponent(panel_7, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
-										.addComponent(panel_17, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)))
-								.addGap(390)))))
+										.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+										.addComponent(lblErrorMessage, GroupLayout.PREFERRED_SIZE, 524, GroupLayout.PREFERRED_SIZE)))))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+							.addGap(164)
+							.addComponent(lblGame)
+							.addGap(591))))
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(322)
 					.addComponent(lblTopTen)
-					.addContainerGap(683, Short.MAX_VALUE))
+					.addContainerGap(853, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_7, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_6, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_17, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
-							.addGap(33)
-							.addComponent(lblGame)
-							.addGap(39)
-							.addComponent(layeredPane_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblGame)
+									.addGap(27))
+								.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+									.addGap(62))
+								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addComponent(panel_19, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(panel_7, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGap(39)
+											.addComponent(layeredPane_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGap(30)
+											.addComponent(lblHallOfFame)))
+									.addGap(41)
+									.addComponent(lblTopTen))
+								.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+									.addPreferredGap(ComponentPlacement.RELATED)
+									.addComponent(panel_10, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGap(63)
+											.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+												.addComponent(panel_6, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+												.addComponent(lblErrorMessage, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE))
+											.addGap(63)
+											.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+												.addGroup(groupLayout.createSequentialGroup()
+													.addComponent(btnLogout)
+													.addGap(26))
+												.addGroup(groupLayout.createSequentialGroup()
+													.addComponent(panel_9, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+													.addPreferredGap(ComponentPlacement.RELATED))))
+										.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+											.addGap(7)
+											.addComponent(panel_5, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+											.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+											.addComponent(panel_8, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+											.addGap(71))))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(12)
+									.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+										.addComponent(panel_24, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+										.addComponent(panel_11, GroupLayout.PREFERRED_SIZE, 296, GroupLayout.PREFERRED_SIZE)))))
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(135)
-							.addComponent(lblHallOfFame)))
-					.addGap(31)
-					.addComponent(lblTopTen)
-					.addPreferredGap(ComponentPlacement.RELATED, 295, Short.MAX_VALUE)
-					.addComponent(lblErrorMessage, GroupLayout.PREFERRED_SIZE, 18, GroupLayout.PREFERRED_SIZE)
-					.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addComponent(btnLogout)
-					.addGap(26)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(panel_16, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(panel_9, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
-						.addComponent(panel_10, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
-						.addComponent(panel_11, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
-						.addComponent(panel_12, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
-						.addComponent(panel_13, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
-						.addComponent(panel_8, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
-						.addComponent(panel_14, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
-						.addComponent(panel_15, GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)))
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(316)
+									.addComponent(panel_21, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+									.addGap(60)
+									.addComponent(panel_23, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(panel, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+									.addGap(61)
+									.addComponent(panel_18, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+									.addGap(60)
+									.addComponent(panel_20, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)
+									.addGap(61)
+									.addComponent(panel_22, GroupLayout.PREFERRED_SIZE, 64, GroupLayout.PREFERRED_SIZE)))
+							.addGap(22)))
+					.addGap(10))
 		);
 		frame.getContentPane().setLayout(groupLayout);
 		frame.setBounds(100, 100, 728, 612);
